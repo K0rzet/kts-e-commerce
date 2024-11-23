a046697e4ac917db5adea8e9a6945185c4d4dbbd
K0rzet <ehot2004@gmail.com>
2024-11-24 01:35:04 +0300

    fix yookassa: try 2

--- src/store/OrderStore.ts
+++ src/store/OrderStore.ts
@@ -70,7 +70,7 @@ export class OrderStore implements ILocalStore {
       }
 
       const script = document.createElement('script');
-      script.src = 'https://yookassa.ru/checkout-widget/v3/checkout-widget.js';
+      script.src = 'https://static.yoomoney.ru/checkout-client/checkout-widget.js';
       script.async = true;
       script.crossOrigin = 'anonymous';
 
