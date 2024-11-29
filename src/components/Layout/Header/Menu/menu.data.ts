994947fb07eb077874a8efb5a522dd1cfec9841c
K0rzet <113698441+K0rzet@users.noreply.github.com>
2024-11-29 14:39:54 +0300

    Update menu.data.ts

--- src/components/Layout/Header/Menu/menu.data.ts
+++ src/components/Layout/Header/Menu/menu.data.ts
@@ -3,24 +3,6 @@ import CartIcon from '../components/CartIcon';
 import UserIcon from '../components/UserIcon';
 import { authStore } from '@/store/AuthStore';
 
-export const menuData = [
-  {
-    id: 1,
-    title: 'Products',
-    path: '/products',
-  },
-  {
-    id: 2,
-    title: 'Categories',
-    path: '/categories',
-  },
-  {
-    id: 3,
-    title: 'About Us',
-    path: '/about-us',
-  },
-];
-
 export const getMenuIconsData = () => [
   {
     id: 1,
